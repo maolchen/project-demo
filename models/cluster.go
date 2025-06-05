e2eba93b3f1170453caaa91e98a5a03c2fd171b6
“maolinchen” <421336911@qq.com>
2025-06-06 00:11:43 +0800

    完成集群增删改查的方法

--- models/cluster.go
+++ models/cluster.go
@@ -1,20 +1,69 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/maolchen/krm-backend/database"
+	"gorm.io/gorm"
+)
 
 type ClusterInfo struct {
 	gorm.Model
-	Name       string `gorm:"unique;not null" json:"name"`
-	Label      string `gorm:"default null" json:"label"` //集群标签
-	Kubeconfig string `gorm:"not null" json:"kubeconfig"`
+	Name       string  `gorm:"unique;not null" json:"name"`
+	Label      *string `gorm:"default:null" json:"label"` //集群标签
+	Kubeconfig string  `gorm:"not null" json:"-`
 }
 
 type ClusterRespoense struct {
 	Name   string            `json:"name"`
-	Label  string            `json:"label"`
-	status map[string]string `json:"status"`
+	Label  *string           `json:"label"`
+	Status map[string]string `json:"status"`
 }
+
+var db = database.GetDB()
 
 func (c *ClusterInfo) TableName() string {
 	return "cluster_info"
+}
+
+// 添加集群
+func (c *ClusterInfo) Insert() error {
+
+	res := db.Create(&c)
+	return res.Error
+
+}
+
+func (c *ClusterInfo) Update(name string, updates map[string]interface{}) error {
+	var cluster ClusterInfo
+	if _, err := c.GetByName(name); err != nil {
+		return err
+	}
+	if newKubeconfig, ok := updates["kubeconfig"]; ok {
+		cluster.Kubeconfig = newKubeconfig.(string)
+	}
+	if newLabel, ok := updates["label"]; ok {
+		labelStr := newLabel.(string)
+		cluster.Label = &labelStr
+	}
+	return db.Save(&cluster).Error
+
+}
+
+func (c *ClusterInfo) Delete(name string) error {
+	var cluster ClusterInfo
+	if _, err := c.GetByName(name); err != nil {
+		return err
+	}
+	return db.Delete(&cluster).Error
+}
+
+func (c *ClusterInfo) GetAll() []ClusterInfo {
+	var ClusterArray []ClusterInfo
+	db.Find(&ClusterArray)
+	return ClusterArray
+}
+
+func (c *ClusterInfo) GetByName(name string) (ClusterInfo, error) {
+	var cluster ClusterInfo
+	err := db.Where("name = ?", name).First(&cluster).Error
+	return cluster, err
 }
--- service/cluster.go
+++ service/cluster.go
@@ -2,9 +2,22 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/maolchen/krm-backend/models"
 	"go.uber.org/zap"
 )
 
-func AddCluster(ctx *gin.Context) {
+func AddCluster(ctx *gin.Context) error {
 	zap.S().Info("添加集群逻辑")
+	// 接收数据
+	clusterInfo := models.ClusterInfo{}
+
+	if err := ctx.ShouldBindJSON(&clusterInfo); err != nil {
+		return err
+	}
+
+	// 校验集群状态
+
+	//数据处理
+	return clusterInfo.Insert()
+	
 }
